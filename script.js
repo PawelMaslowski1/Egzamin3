6b83d996deec941d815b2b18bffc2ba6851c44a9
PawelMaslowski1 <127610603+PawelMaslowski1@users.noreply.github.com>
2023-03-28 09:19:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const tytulElement = document.getElementById("title");
+tytulElement.textContent = "Egzamin część III ";
+console.log(tytulElement);
+
+const podtytulElement = document.querySelector(".subtitle");
+podtytulElement.style.color = "red";
+podtytulElement.innerText = "poziom ZAAWANSOWANY";
+console.log(podtytulElement);
+
+const imieEl = document.querySelector(".imieEl");
+imieEl.innerText = "Paweł";
+console.log(imieEl);
+
+const nazwiskoEl = document.querySelector(".nazwiskoEl");
+nazwiskoEl.innerText = "Masłowski";
+console.log(nazwiskoEl);
